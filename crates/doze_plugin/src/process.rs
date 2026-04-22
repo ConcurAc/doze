51c79f6b041a29617a91d39d42eb5a8046fd293d
Connor Davis <connor@scequ.com>
2026-04-22 18:53:38 +1000

    fix(plugin): impl reader and writer iters

--- crates/doze_plugin/src/process.rs
+++ crates/doze_plugin/src/process.rs
@@ -7,7 +7,27 @@ use crate::events::{Event, EventSender, HostEvent, PluginEvent};
 
 const MAX_CONSTANT_CHANNEL: usize = 64;
 
+pub struct Process<'h, 'p> {
+    pub frames: usize,
+    pub steady_time: i64,
+
+    pub audio_inputs: &'h dyn AudioBus<'h>,
+    pub audio_outputs: &'h dyn AudioBus<'h>,
+
+    pub events: &'h mut dyn Iterator<Item = Event<HostEvent>>,
+    pub sender: &'h mut dyn EventSender<Event = Event<PluginEvent<'p>>>,
+}
+
+pub enum Status {
+    Continue,
+    ContinueIfNotQuiet,
+    Tail,
+    Sleep,
+    Error,
+}
+
 pub trait AudioBus<'h> {
+    fn count(&self) -> usize;
     fn get_f32_buffer(&self, index: usize) -> Result<AudioBuffer<'h, f32>, AudioBufferError>;
     fn get_f64_buffer(&self, index: usize) -> Result<AudioBuffer<'h, f64>, AudioBufferError>;
 }
@@ -20,6 +40,9 @@ pub struct AudioBuffer<'p, T: Sample> {
 }
 
 impl<'p, T: Sample> AudioBuffer<'p, T> {
+    pub fn count(&self) -> usize {
+        self.channels.len()
+    }
     pub fn get_reader(&self, channel: usize) -> Option<SliceReader<'p, T>> {
         let buffer = self
             .channels
@@ -35,6 +58,18 @@ impl<'p, T: Sample> AudioBuffer<'p, T> {
             .map(|&d| unsafe { core::slice::from_raw_parts_mut(d, self.frames) })?;
 
         Some(buffer.into())
+    }
+    pub fn iter_reader(&self) -> ReaderIter<'_, T> {
+        ReaderIter {
+            buffer: self,
+            channel: 0,
+        }
+    }
+    pub fn iter_writer(&'p mut self) -> WriterIter<'p, T> {
+        WriterIter {
+            buffer: self,
+            channel: 0,
+        }
     }
     pub fn set_constant(&mut self, channel: usize) {
         debug_assert!(channel < MAX_CONSTANT_CHANNEL);
@@ -77,28 +112,39 @@ impl<'h, T: Sample> AudioBuffer<'h, T> {
     }
 }
 
+pub struct ReaderIter<'b, T: Sample> {
+    buffer: &'b AudioBuffer<'b, T>,
+    channel: usize,
+}
+
+impl<'b, T: Sample> Iterator for ReaderIter<'b, T> {
+    type Item = SliceReader<'b, T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let reader = self.buffer.get_reader(self.channel);
+        self.channel += 1;
+        reader
+    }
+}
+
+pub struct WriterIter<'b, T: Sample> {
+    buffer: &'b mut AudioBuffer<'b, T>,
+    channel: usize,
+}
+
+impl<'b, T: Sample> Iterator for WriterIter<'b, T> {
+    type Item = SliceWriter<'b, T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let writer = self.buffer.get_writer(self.channel);
+        self.channel += 1;
+        writer
+    }
+}
+
 pub enum AudioBufferError {
     NotFound,
     IsF32,
     IsF64,
     Null,
-}
-
-pub struct Process<'h, 'p> {
-    pub frames: usize,
-    pub steady_time: i64,
-
-    pub audio_inputs: &'h dyn AudioBus<'h>,
-    pub audio_outputs: &'h dyn AudioBus<'h>,
-
-    pub events: &'h mut dyn Iterator<Item = Event<HostEvent<'h>>>,
-    pub sender: &'h mut dyn EventSender<Event = Event<PluginEvent<'p>>>,
-}
-
-pub enum Status {
-    Continue,
-    ContinueIfNotQuiet,
-    Tail,
-    Sleep,
-    Error,
 }
